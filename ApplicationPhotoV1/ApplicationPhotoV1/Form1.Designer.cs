302d80687e4aa3635c907bc8de4ebf443aeb1891
Gildax13 <60438399+Gildax13@users.noreply.github.com>
2022-10-18 23:39:46 +0200

    Ajout code appli

---
+++ ApplicationPhotoV1/ApplicationPhotoV1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ApplicationPhotoV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addImage = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.generateImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addImage
+            // 
+            this.addImage.Location = new System.Drawing.Point(691, 15);
+            this.addImage.Name = "addImage";
+            this.addImage.Size = new System.Drawing.Size(186, 23);
+            this.addImage.TabIndex = 0;
+            this.addImage.Text = "Ajouter les photos";
+            this.addImage.UseVisualStyleBackColor = true;
+            this.addImage.Click += new System.EventHandler(this.addImage_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(904, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Resize photo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(647, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Largeur de la photo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hauteur de la photo";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(773, 72);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(773, 101);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(601, 371);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(633, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(1032, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 319);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // generateImage
+            // 
+            this.generateImage.Location = new System.Drawing.Point(647, 196);
+            this.generateImage.Name = "generateImage";
+            this.generateImage.Size = new System.Drawing.Size(370, 181);
+            this.generateImage.TabIndex = 9;
+            this.generateImage.Text = "Cliquer pour générer l\'image";
+            this.generateImage.UseVisualStyleBackColor = true;
+            this.generateImage.Click += new System.EventHandler(this.generateImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1356, 555);
+            this.Controls.Add(this.generateImage);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addImage);
+            this.Name = "Form1";
+            this.Text = "Créateur de grande photo";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button addImage;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private OpenFileDialog openFileDialog1;
+        private ListBox listBox1;
+        private Button generateImage;
+    }
+}
---
+++ ApplicationPhotoV1/ApplicationPhotoV1/Form1.cs
@@ -0,0 +1,244 @@
+using System.Windows.Forms;
+using System;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
+
+namespace ApplicationPhotoV1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap[] images;
+        private int currentImage;
+        private int imageCount = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeOpenFileDialog();
+            this.setImages();
+        }
+        private void InitializeOpenFileDialog()
+        {
+            // Allow the user to select multiple images.
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "My Image Browser";
+        }
+        private void swapImageFromToOrigin(int from, int to)
+        {
+
+            if (from < 0 || from >= images.Length || to < 0 || to >= images.Length)
+                return;
+            if (from > to)
+            {
+                for (int i = to; i < from; i++)
+                {
+                    swapImageFromTo(i, i - 1);
+                }
+            }
+            else
+            {
+                for (int i = to; i > from; i--)
+                {
+                    swapImageFromTo(i, i + 1);
+                }
+            }
+        }
+
+        private void swapImageFromTo(int from, int to)
+        {
+
+            if (from < 0 || from >= images.Length || to < 0 || to >= images.Length)
+                return;
+            Bitmap tmp = images[from];
+            images[from] = images[to];
+            images[to] = tmp;
+        }
+        private void addImage_Click(object sender, EventArgs e) //Add photos
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (String imageDir in openFileDialog1.FileNames)
+                    {
+
+                        Bitmap image = new Bitmap(imageDir);
+                        image = ResizeBitmap(image,image.Width/4,image.Height/4);
+                        Array.Resize(ref images, images.Length + 1);
+
+                        images[currentImage++] = image;
+
+                    }
+                    updateListBox();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+
+            return result;
+        }
+        private void updateListBox()
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < images.Length; i++)
+            {
+                if (images[i] != null)
+                {
+                    Bitmap image = images[i];
+                    listBox1.Items.Add("Image " + i + " : " + image.Width + "x" + image.Height);
+                }
+
+            }
+        }
+
+        public Bitmap[] getImages()
+        {
+            return images;
+        }
+
+        public void setImages()
+        {
+            images = new Bitmap[imageCount];
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) //Largeur
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e) //Hauteur
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Lancer la crééation
+        {
+            if (pictureBox1.Image != null)
+            {
+                updateImage();
+            }
+
+        }
+
+        private int getWidthOfImage(Bitmap image)
+        {
+            // Get the width of the image
+            int width = image.Width;
+
+            return width;
+        }
+
+        private int getHeightOfImage(Bitmap image)
+        {
+            // Get the height of the image
+            int height = image.Height;
+            return height;
+        }
+
+        private void setValueOnForm()
+        {
+            Bitmap image = new Bitmap(pictureBox1.ImageLocation);
+            int width = getWidthOfImage(image);
+            int height = getHeightOfImage(image);
+
+
+
+            numericUpDown1.Value = width;
+            numericUpDown2.Value = height;
+
+            textBox1.Text = "L = " + width + " H = " + height;
+
+
+
+        }
+        private void updateImage()
+        {
+            Bitmap image = (Bitmap)pictureBox1.Image;
+            int width = (int)numericUpDown1.Value;
+            int height = (int)numericUpDown2.Value;
+            Bitmap newImage = new Bitmap(image, width, height);
+            pictureBox1.Image = newImage;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = images[listBox1.SelectedIndex];
+
+        }
+
+        private void generateImage_Click(object sender, EventArgs e)
+        {
+            downloadImage();
+        }
+        private Bitmap getBitmapFromImage()
+        {
+            Bitmap image = images[0];
+            if (image != null)
+            {
+                Bitmap bitmap = null;
+                using (var stream = new MemoryStream())
+                {
+                    stream.Position = 0;
+                    bitmap = new Bitmap(image.Width * (images.Length - 1), image.Height);
+                }
+                if (bitmap == null)
+                    return null;
+                Graphics g = Graphics.FromImage(bitmap);
+                for (int i = 0; i < images.Length; i++)
+                {
+                    image = images[i];
+                    if (image != null)
+                    {
+                        g.DrawImage(image, i * image.Width, 0);
+                    }
+                }
+                g.Dispose();
+                return bitmap;
+            }
+            return null;
+        }
+
+        private Bitmap appendImageOnRight(Bitmap image1, Bitmap image2)
+        {
+            if (image1 != null && image2 != null)
+            {
+                Bitmap bitmap = new Bitmap(image1.Width + image2.Width, image1.Height);
+                Graphics g = Graphics.FromImage(bitmap);
+                g.DrawImage(image1, 0, 0);
+                g.DrawImage(image2, image1.Width, 0);
+                g.Dispose();
+                return bitmap;
+            }
+            return null;
+        }
+        private void downloadImage()
+        {
+            Bitmap bitmap = getBitmapFromImage();
+            if (bitmap != null)
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        bitmap.Save(dialog.FileName);
+                    }
+                    catch(Exception e) {
+                        textBox1.Text = e.Message;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
